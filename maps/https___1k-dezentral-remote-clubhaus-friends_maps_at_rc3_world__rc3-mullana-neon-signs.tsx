a7c90b20054579c4728ce4ab832e7609e9163765
Torsten Frenzel <github@focn.de>
2021-02-01 11:32:31 +0100

    erste Tests

---
+++ maps/https___1k-dezentral-remote-clubhaus-friends_maps_at_rc3_world__rc3-mullana-neon-signs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="https___1k-dezentral-remote-clubhaus-friends_maps_at_rc3_world__rc3-mullana-neon-signs" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="cache/https___1k-dezentral-remote-clubhaus-friends_maps_at_rc3_world__rc3-mullana-neon-signs.png" width="512" height="512"/>
+</tileset>
